1eca537d000aa75bbfcbd7aeaa1ed01188dcfb73
Ichitaro Yamazaki <iyamaza@lightsaber.srn.sandia.gov>
2024-12-19 22:42:01 -0700

    Tacho : additional options for Matlab interface
    Signed-off-by: Ichitaro Yamazaki <iyamaza@lightsaber.srn.sandia.gov>

--- packages/shylu/shylu_node/tacho/matlab/bin/tachoMex.cpp
+++ packages/shylu/shylu_node/tacho/matlab/bin/tachoMex.cpp
@@ -22,7 +22,10 @@ extern void _main();
 namespace Tacho {
 
 template <>
-TachoSystem<double>::TachoSystem() {}
+TachoSystem<double>::TachoSystem() :
+ verbose(false),
+ dofs_per_node(1)
+{}
 template <>
 TachoSystem<double>::~TachoSystem() {}
 
@@ -33,20 +36,32 @@ template <typename Scalar>
 int TachoSystem<Scalar>::option(const mxArray** mx) {
   std::string option_name = loadDataFromMatlab<std::string>(mx[0]);
   if (option_name == "verbose") {
-    bool verbose = true;
+    verbose = true;
     solver.setVerbose(verbose);
+    if (verbose) printf( " > option(verbose)\n" );
   } else if (option_name == "method") {
     int method = 1;
     std::string method_name = loadDataFromMatlab<std::string>(mx[1]);
+    if (verbose) printf( " > option(method=%s)\n",method_name.c_str() );
     if (method_name == "chol")
       method = 1;
     else if (method_name == "ldl")
       method = 2;
     else if (method_name == "lu")
       method = 3;
+    else if (method_name == "sk")
+      method = 5;
     else {
       std::cout << "Error: not supported solution method\n";
-  }
+    }
+    solver.setSolutionMethod(method);
+  } else if (option_name == "dofs-per-node") {
+    dofs_per_node = loadDataFromMatlab<int>(mx[1]);
+    if (verbose) printf( " > option(dofs-per-node=%d)\n",dofs_per_node );
+  } else if (option_name == "small-problem-thres") {
+    int small_problem_thres = loadDataFromMatlab<int>(mx[1]);
+    if (verbose) printf( " > option(small-problem-thres=%d)\n",small_problem_thres );
+    solver.setSmallProblemThresholdsize(small_problem_thres);
   }
   return IS_TRUE;
 }
@@ -55,17 +70,36 @@ int TachoSystem<Scalar>::option(const mxArray** mx) {
 /// Setup function
 template <typename Scalar>
 int TachoSystem<Scalar>::setup(const mxArray* mx) {
-  // decide whether do do default or custom setup
-  CrsMatrixBaseTypeHost A;
   loadMatrixFromMatlab<double, host_device_type>(mx, A);
   try {
-    printf( " solveer.analyze\n" );
-    solver.analyze(A.NumRows(), A.RowPtr(), A.Cols());
-    printf( " solveer.initialize\n" );
+    if (verbose) printf( " solver.analyze\n" );
+    if (dofs_per_node > 1) {
+      solver.analyze(A.NumRows(), dofs_per_node, A.RowPtr(), A.Cols());
+    } else {
+      solver.analyze(A.NumRows(), A.RowPtr(), A.Cols());
+    }
+    if (verbose) printf( " solver.initialize\n" );
     solver.initialize();
-    printf( " setup done\n" );
+    if (verbose) printf( " setup done\n" );
   } catch (std::exception& e) {
-    std::cout << "An error occurred during Tpetra custom problem setup:" << std::endl;
+    std::cout << "An error occurred during TachoMex setup:" << std::endl;
+    std::cout << e.what() << std::endl;
+    return IS_FALSE;
+  }
+  return IS_TRUE;
+}
+
+
+/// Setup function
+template <typename Scalar>
+int TachoSystem<Scalar>::factor(const mxArray* mx) {
+  loadMatrixFromMatlab<double, host_device_type>(mx, A);
+  try {
+    if (verbose) printf( " solver.factorize\n" );
+    solver.factorize(A.Values());
+    if (verbose) printf( " factorize done\n" );
+  } catch (std::exception& e) {
+    std::cout << "An error occurred during TachoMex setup:" << std::endl;
     std::cout << e.what() << std::endl;
     return IS_FALSE;
   }
@@ -82,34 +116,16 @@ mxArray* TachoSystem<Scalar>::solve(const mxArray* mx) {
   DenseMultiVectorType t ("t", b.extent(0), b.extent(1));
   mxArray* output;
   try {
-    printf( " solveer.solve\n" );
+    if (verbose) printf( " solver.solve\n" );
     this->solver.solve(x, b, t);
-    printf( " solveer.solve done\n" );
+    if (verbose) printf( " solver.solve done\n" );
     output = saveVectorsToMatlab<double, host_device_type>(x);
-    printf( " solution saved\n" );
+    if (verbose) printf( " solution saved\n" );
   } catch (std::exception& e) {
-    mexPrintf("Error occurred while running Belos solver:\n");
+    mexPrintf("Error occurred during TachoMex solve:\n");
     std::cout << e.what() << std::endl;
     output = mxCreateDoubleScalar(0);
   }
-  return output;
-}
-
-
-/// Apply function
-template <typename Scalar>
-mxArray* TachoSystem<Scalar>::apply(const mxArray* mx) {
-  //DenseMultiVectorType b = loadDataFromMatlab<DenseMultiVectorType>(mx);
-  //DenseMultiVectorType x ("x", b.extent(0), b.extent(1));
-  //DenseMultiVectorType t ("t", b.extent(0), b.extent(1));
-  mxArray* output;
-  try {
-    //this->solver.solve(x, b, t);
-  } catch (std::exception& e) {
-    mexPrintf("Error occurred while applying MueLu-Tpetra preconditioner:\n");
-    std::cout << e.what() << std::endl;
-  }
-  //output = saveDataToMatlab(x);
   return output;
 }
 
@@ -130,20 +146,19 @@ MODE_TYPE sanity_check(int nrhs, const mxArray* prhs[]) {
         mexErrMsgTxt("TachoMex Error: Invalid input for setup phase\n");
       }
       break;
+    case MODE_FACTOR:
+      if(nrhs == 2) {
+        rv = MODE_FACTOR;
+      } else {
+        mexErrMsgTxt("TachoMex Error: Invalid input for factor phase\n");
+      }
+      break;
     case MODE_SOLVE:
       // problem ID and matrix or rhs must be numeric
       if(nrhs == 2) {
         rv = MODE_SOLVE;
       } else {
         mexErrMsgTxt("TachoMex Error: Invalid input for solve phase\n");
-      }
-      break;
-    case MODE_APPLY:
-      // problem ID and RHS must be numeric
-      if(nrhs == 2) {
-        rv = MODE_APPLY;
-      } else {
-        mexErrMsgTxt("TachoMex Error: Invalid input for apply phase\n");
       }
       break;
     case MODE_CLEANUP:
@@ -162,7 +177,7 @@ MODE_TYPE sanity_check(int nrhs, const mxArray* prhs[]) {
       }
       break;
      default:
-      printf("Mode number = %d\n", (int)mode);
+      printf("Mode number = %d (nrhs = %d)\n", (int)mode, (int)nrhs);
       mexErrMsgTxt("Error: Invalid input mode\n");
   };
   return rv;
@@ -203,6 +218,21 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
       }
       break;
     }
+    case MODE_FACTOR: {
+      try {
+        printf( " -- Factor --\n" );
+        rv =  dp.factor(prhs[1]);
+	if (nlhs > 0) {
+          plhs[0] = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
+          *((int*)mxGetData(plhs[0])) = rv;
+	}
+        printf( " -- Factor done --\n" );
+      } catch (std::exception& e) {
+        mexPrintf("An error occurred during factor routine:\n");
+        std::cout << e.what() << std::endl;
+      }
+      break;
+    }
     case MODE_SOLVE: {
       try {
         // get pointer to MATLAB array that will be "B" or "rhs" multivector
@@ -215,19 +245,6 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
         printf( " -- Solve done! --\n\n" );
       } catch (std::exception& e) {
         mexPrintf("An error occurred during the solve routine:\n");
-        std::cout << e.what() << std::endl;
-      }
-      break;
-    }
-    case MODE_APPLY: {
-      try {
-        // MODE_APPLY, probID, rhsVec
-        // prhs[0] holds the MODE_APPLY enum value
-        printf( " -- Apply --\n" );
-        plhs[0] = dp.apply(prhs[1]);
-        printf( " -- Apply done --\nn\n" );
-      } catch (std::exception& e) {
-        mexPrintf("An error occurred during the apply routine:\n");
         std::cout << e.what() << std::endl;
       }
       break;
@@ -245,7 +262,7 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
       try {
         rv =  dp.option(&prhs[1]);
       } catch (std::exception& e) {
-        mexPrintf("An error occurred during the cleanup routine:\n");
+        mexPrintf("An error occurred during the option routine:\n");
         std::cout << e.what() << std::endl;
       }
       break;
