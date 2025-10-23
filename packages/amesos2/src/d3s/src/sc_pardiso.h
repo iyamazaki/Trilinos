3d7ed8415959158a2555a0e4a9c1265181319d39
iyamazaki <iyamaza@sandia.gov>
2025-10-23 15:19:46 -0600

    Amesos2 D3S : return error code
    Signed-off-by: iyamazaki <iyamaza@sandia.gov>

--- packages/amesos2/src/d3s/src/sc_pardiso.h
+++ packages/amesos2/src/d3s/src/sc_pardiso.h
@@ -16,44 +16,45 @@ public:
 
   ~sc_pardiso();
 
-  void initialize(const int numRows,
-                  int* rowBegin,
-                  int* columns,
-                  int numRowsB,
-                  int* rowsB,
-                  int msg_level,
-                  int num_threads,
-                  int reorder_option,
-                  int structurally_symmetric,
-                  int print_level);
-  
-  void factorize(double* values);
-  
+  int initialize(const int numRows,
+                 int* rowBegin,
+                 int* columns,
+                 int numRowsB,
+                 int* rowsB,
+                 int msg_level,
+                 int num_threads,
+                 int reorder_option,
+                 int structurally_symmetric,
+                 int debug_level,
+                 bool verbose);
+
+  int factorize(double* values, bool verbose);
+
   std::vector<double> getSchurComplement() const;
-  
+
   int cleanup();
-  
+
   void setIparam(int idx, int ivalue);
-  
+
   int getIparam(int idx) const;
-  
+
   void setMatrixType(int matrixType);
-  
+
   void setMessageLevel(int messageLevel);
-  
+
   void refactorMatrix(double* values);
-  
+
   int solve(double* rhs,
             double* sol,
             const int phase);
-  
+
   std::vector<double> getTimings();
-  
+
   int getNumRows()
   {
     return m_numRows;
   }
-  
+
 private:
   
   inline double clockIt();
@@ -62,14 +63,17 @@ private:
   
   void set_parameters(const int matrix_type,
                       const int msg_level,
-                      const int reorder_option);
+                      const int reorder_option,
+                      const bool verbose);
   
   int analysis_phase();
   
   int numeric_phase();
+
+private:
   
   int m_numRows = 0;
-  int m_print_level = 0;
+  int m_debug_level = 0;
   int *m_rowBegin, *m_columns;
   double *m_values;
   int m_numRowsB, *m_rowsB, m_phase=-1;
@@ -82,5 +86,4 @@ private:
   std::vector<int> m_perm;
 
 };
-
 
