c06687c3ce520c6761ab8e95f1f93247a679777c
iyamazaki <iyamaza@sandia.gov>
2026-01-16 21:45:28 -0700

    Tacho: add more options for testRocSolver  * synch before checking residual norm  * add outer iteration loop  * option to use different block sizes
    Signed-off-by: iyamazaki <iyamaza@sandia.gov>

--- packages/shylu/shylu_node/tacho/example/Tacho_TestRocSolver.cpp
+++ packages/shylu/shylu_node/tacho/example/Tacho_TestRocSolver.cpp
@@ -7,100 +7,25 @@
 
 int main(int argc, char **argv) {
   int verbose = 0;
-  int n  = 10; // matrix dimension
+  int niters = 200; // number of outer iterations
   int nb = 10; // number of batches
-  int nstreams = 1; // number of streams
+  int N1 = 100, N2 = 1000;  // matrix dimension
+  int nstreams = 16; // number of streams
   int option = 0;
+  char* filename = nullptr;
   bool fence = false;
   for (int i=0; i<argc; i++) {
-    if (0 == strcmp(argv[i], "--verbose")) verbose = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--option"))  option = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--fence"))   fence = true;
-    if (0 == strcmp(argv[i], "--dim"))     n = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--nb"))      nb = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--fence"))    fence = true;
+    if (0 == strcmp(argv[i], "--verbose"))  verbose = 1;
+    if (0 == strcmp(argv[i], "--file"))     filename = argv[++i];
+    if (0 == strcmp(argv[i], "--option"))   option = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--n1"))       N1 = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--n2"))       N2 = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--nb"))       nb = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--niters"))   niters = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--nstreams")) nstreams = atoi(argv[++i]);
   }
-  printf( "\n n=%d, nd=%d, nstreams=%d, option=%d\n",n,nb,nstreams,option );
-
-  const rocblas_int N = n;
-  const rocblas_int nrhs = 1;
-  const rocblas_int lda = N;
-  const rocblas_int lde = N;
-  const rocblas_int ldd = N;
-  const rocblas_int ldb = N;
-
-  // Solving "nb" 2-by-2 SPD linear systems [A E; E' D]
-  // > on host
-  double **hA = (double**)malloc(nb * sizeof(double*));
-  double **hE = (double**)malloc(nb * sizeof(double*));
-  double **hD = (double**)malloc(nb * sizeof(double*));
-  // > on device
-  double **dA = (double**)malloc(nb * sizeof(double*));
-  double **dE = (double**)malloc(nb * sizeof(double*));
-  double **dD = (double**)malloc(nb * sizeof(double*));
-  // vectors
-  double **hB = (double**)malloc(nb * sizeof(double*));
-  double **hX = (double**)malloc(nb * sizeof(double*));
-  double **dB = (double**)malloc(2*nb * sizeof(double*));
-  for (int k=0; k<nb; k++) {
-    // Generate SPD matrices
-    hA[k] = (double*)malloc((N*N) * sizeof(double));
-    hE[k] = (double*)malloc((N*N) * sizeof(double));
-    hD[k] = (double*)malloc((N*N) * sizeof(double));
-    for (rocblas_int i=0; i<N; i++) {
-      // A & D, diagonal blocks
-      for (rocblas_int j=i; j<N; j++) {
-        hA[k][i+j*N] = double(std::rand()) / double(RAND_MAX);
-        hD[k][i+j*N] = double(std::rand()) / double(RAND_MAX);
-      }
-      hA[k][i+i*N] += 2*N;
-      hD[k][i+i*N] += 2*N;
-      // E, off-diagonal block
-      for (rocblas_int j=0; j<N; j++) {
-        hE[k][i+j*N] = double(std::rand()) / double(RAND_MAX);
-      }
-    }
-    // explicitly symmetrize the diagonal block
-    // (to simply "check" the residual norms)
-    for (rocblas_int i=0; i<N; i++) {
-      for (rocblas_int j=0; j<i; j++) {
-        hA[k][i+j*N] = hA[k][j+i*N];
-        hD[k][i+j*N] = hD[k][j+i*N];
-      }
-    }
-
-    // Generate RHS, B = A*ones(n,1)
-    hB[k] = (double*)malloc(2*N * sizeof(double));
-    hX[k] = (double*)malloc(2*N * sizeof(double));
-    for (rocblas_int i=0; i<N; i++) {
-      hB[k][i] = 0.0;
-      for (rocblas_int j=0; j<N; j++) {
-        hB[k][i] += hA[k][i+j*N];
-      }
-      for (rocblas_int j=0; j<N; j++) {
-        hB[k][i] += hE[k][i+j*N];
-      }
-    }
-    for (rocblas_int i=0; i<N; i++) {
-      hB[k][N+i] = 0.0;
-      for (rocblas_int j=0; j<N; j++) {
-        hB[k][N+i] += hD[k][i+j*N];
-      }
-      for (rocblas_int j=0; j<N; j++) {
-        hB[k][N+i] += hE[k][j+i*N];
-      }
-    }
-    // Copy matrix & vector from host to device
-    // > matrix
-    hipMalloc((void**)&(dA[k]), sizeof(double) * N*N);
-    hipMalloc((void**)&(dE[k]), sizeof(double) * N*N);
-    hipMalloc((void**)&(dD[k]), sizeof(double) * N*N);
-    hipMemcpy(dA[k], hA[k], sizeof(double) * N*N, hipMemcpyHostToDevice);
-    hipMemcpy(dE[k], hE[k], sizeof(double) * N*N, hipMemcpyHostToDevice);
-    hipMemcpy(dD[k], hD[k], sizeof(double) * N*N, hipMemcpyHostToDevice);
-    // > vector
-    hipMalloc((void**)&(dB[k]), sizeof(double) * 2*N);
-    hipMemcpy(dB[k], hB[k], sizeof(double) * 2*N, hipMemcpyHostToDevice);
-  } // End of generating matrix & vectors
+  printf( "\n n=%d+%d, nd=%d, nstreams=%d, option=%d %s\n",N1,N2,nb,nstreams,option,filename );
 
   // Create rocSOLVER/BLAS handle
   rocblas_handle *handles = (rocblas_handle*)malloc(nstreams * sizeof(rocblas_handle));
@@ -116,122 +41,275 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Allocate integer device integer-array to return error code
+  // Solving "nb" 2-by-2 SPD linear systems [A E; E' D]
+  // > on host
+  double **hA = (double**)malloc(nb * sizeof(double*));
+  double **hE = (double**)malloc(nb * sizeof(double*));
+  double **hD = (double**)malloc(nb * sizeof(double*));
+  // > on device
+  double **dA = (double**)malloc(nb * sizeof(double*));
+  double **dE = (double**)malloc(nb * sizeof(double*));
+  double **dD = (double**)malloc(nb * sizeof(double*));
+  // vectors
+  const rocblas_int nrhs = 1;
+  double **hB = (double**)malloc(nb * sizeof(double*));
+  double **hX = (double**)malloc(nb * sizeof(double*));
+  double **dB = (double**)malloc(nb * sizeof(double*));
+  // info
   rocblas_int *dInfo;
   hipMalloc((void**)&dInfo, nb*sizeof(rocblas_int));
+  // block sizes
+  int *n1 = (int*)malloc(nb * sizeof(int));
+  int *n2 = (int*)malloc(nb * sizeof(int));
 
-  // !!! factor the 2-by-2 blocks !!!
-  double  one( 1.0);
-  double mone(-1.0);
-  for (int k=0; k<nb; k++) {
-    // factor the first diagonal blocks, R := chol(A)
-    int qid = k%nstreams;
-    rocsolver_dpotrf(handles[qid], rocblas_fill_upper, N, dA[k], lda, &dInfo[k]);
+  // Setup matrices/vectors
+  FILE *fp = nullptr;
+  if (filename) {
+    printf( " READING block sizes from %s\n",filename );
+    fp = fopen(filename,"r");
   }
-  if (fence) hipDeviceSynchronize();
   for (int k=0; k<nb; k++) {
-    // compute the off-diagonal factor, E := R^{-1}*E
-    int qid = k%nstreams;
-    rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                  rocblas_operation_transpose, rocblas_diagonal_non_unit,
-                  N, N, &one, dA[k], lda, dE[k], lde);
-    // update the second diagonal, D -= E'*E
-    rocblas_dsyrk(handles[qid], rocblas_fill_upper, rocblas_operation_transpose,
-                  N, N, &mone, dE[k], lde, &one, dD[k], ldd);
-  }
-  if (fence) hipDeviceSynchronize();
-  for (int k=0; k<nb; k++) {
-    // factor the second diagonal block, chol(D)
-    int qid = k%nstreams;
-    rocsolver_dpotrf(handles[qid], rocblas_fill_upper, N, dD[k], ldd, &dInfo[k]);
-  }
-
-  // !!! check (on stream-0) !!!
-  hipDeviceSynchronize();
-
-  // Solve [A,E; E' D] [x1;x2] = [b1;b2]
-  for (int k=0; k<nb; k++) {
-    int qid = 0; //k%nstreams;
-    // --- step 1 --
-    // b(1) := L(A)\b(1)
-    rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                  rocblas_operation_transpose, rocblas_diagonal_non_unit,
-                  N, nrhs, &one, dA[k], lda, &dB[k][0], ldb);
-
-    // --- step 2 --
-    // b(2) := b(2) - E'*b(1)
-    rocblas_dgemm(handles[qid], rocblas_operation_transpose, rocblas_operation_none,
-		  N, nrhs, N, &mone, dE[k], lde, &dB[k][0], ldb, &one, &dB[k][N], ldb);
-
-    // --- step 3 --
-    // b(2) = L(D)\b(2)
-    rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                  rocblas_operation_transpose, rocblas_diagonal_non_unit,
-                  N, nrhs, &one, dD[k], ldd, &dB[k][N], ldb);
-    // x(2) = U(D)\x(2)
-    rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                  rocblas_operation_none, rocblas_diagonal_non_unit,
-                  N, nrhs, &one, dD[k], ldd, &dB[k][N], ldb);
-
-    // --- step 4 --
-    // b(1) = b(1) - E*x(2)
-    rocblas_dgemm(handles[qid], rocblas_operation_none, rocblas_operation_none,
-		  N, nrhs, N, &mone, dE[k], lde, &dB[k][N], ldb, &one, &dB[k][0], ldb);
-
-    // --- step 5 --
-    // x(1) = U(A)\b(1)
-    rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                  rocblas_operation_none, rocblas_diagonal_non_unit,
-                  N, nrhs, &one, dA[k], lda, &dB[k][0], ldb);
-  }
-  // Compute the residual norm
-  for (int k=0; k<nb; k++) {
-    // Copy solution and error code to host
-    rocblas_int hInfo;
-    hipMemcpy(hX[k], dB[k], sizeof(double) * 2*N, hipMemcpyDeviceToHost);
-    hipMemcpy(&hInfo, &dInfo[k], sizeof(rocblas_int), hipMemcpyDeviceToHost);
-    if (verbose == 1) for (int i = 0; i < 2*N; ++i) printf("%e\n",hX[k][i]);
-
-    if (hInfo == 0) {
-      double rnorm = 0.0;
-      double bnorm = 0.0;
-      printf("%d: Checking residual norms:\n",k);
-      // residual norm for the first block row:
-      for (int i = 0; i < N; ++i) {
-        bnorm += hB[k][i]*hB[k][i];
-        for (int j = 0; j < N; ++j) {
-          hB[k][i] -= hA[k][i+j*N] * hX[k][j];
-        }
-        for (int j = 0; j < N; ++j) {
-          hB[k][i] -= hE[k][i+j*N] * hX[k][N+j];
-        }
-        rnorm += hB[k][i]*hB[k][i];
-      }
-      double rnorm1 = std::sqrt(rnorm);
-      double bnorm1 = std::sqrt(bnorm);
-      printf( " 1: rnorm = %e / %e = %e\n",rnorm1,bnorm1,rnorm1/bnorm1 );
-      // residual norm for the second block row:
-      for (int i = 0; i < N; ++i) {
-        bnorm += hB[k][N+i]*hB[k][N+i];
-        for (int j = 0; j < N; ++j) {
-          hB[k][N+i] -= hE[k][j+i*N] * hX[k][j];
-        }
-        for (int j = 0; j < N; ++j) {
-          hB[k][N+i] -= hD[k][i+j*N] * hX[k][N+j];
-        }
-        rnorm += hB[k][N+i]*hB[k][N+i];
-      }
-
-      rnorm = std::sqrt(rnorm);
-      bnorm = std::sqrt(bnorm);
-      printf( " 2: rnorm = %e / %e = %e\n",rnorm,bnorm,rnorm/bnorm );
-      double tol = 0.000000001;
-      if (rnorm > tol) printf("%d: FAIL\n",k);
-      else printf("%d: PASS\n",k);
+    // Matrix dimension
+    if (fp) {
+      fscanf(fp,"%d %d\n",&n1[k],&n2[k]);
     } else {
-      printf("Cholesky factorization failed. Info code: %d\n", hInfo);
+      n1[k] = N1;
+      n2[k] = N2;
     }
-    printf("\n");
+    // Generate SPD matrices [A E; E' D]
+    hA[k] = (double*)malloc((n1[k]*n1[k]) * sizeof(double));
+    hE[k] = (double*)malloc((n1[k]*n2[k]) * sizeof(double));
+    hD[k] = (double*)malloc((n2[k]*n2[k]) * sizeof(double));
+
+    // A, first diagonal blocks, and E, off-diagonal block
+    for (rocblas_int i=0; i<n1[k]; i++) {
+      // A, first diagonal block,  n1-by-n1
+      for (rocblas_int j=i; j<n1[k]; j++) {
+        hA[k][i+j*n1[k]] = double(std::rand()) / double(RAND_MAX);
+      }
+      hA[k][i+i*n1[k]] += (n1[k]+n2[k]);
+      // E, off-diagonal block, n1-by-n2
+      for (rocblas_int j=0; j<n2[k]; j++) {
+        hE[k][i+j*n1[k]] = double(std::rand()) / double(RAND_MAX);
+      }
+    }
+    // D, second diagonal block, and off-diagonal block
+    for (rocblas_int i=0; i<n2[k]; i++) {
+      // D, second diagonal blocks, n2-by-n2
+      for (rocblas_int j=i; j<n2[k]; j++) {
+        hD[k][i+j*n2[k]] = double(std::rand()) / double(RAND_MAX);
+      }
+      hD[k][i+i*n2[k]] += (n1[k]+n2[k]);
+    }
+    // explicitly symmetrize the diagonal block
+    // (to simply "check" the residual norms)
+    for (rocblas_int i=0; i<n1[k]; i++) {
+      for (rocblas_int j=0; j<i; j++) {
+        hA[k][i+j*n1[k]] = hA[k][j+i*n1[k]];
+      }
+    }
+    for (rocblas_int i=0; i<n2[k]; i++) {
+      for (rocblas_int j=0; j<i; j++) {
+        hD[k][i+j*n2[k]] = hD[k][j+i*n2[k]];
+      }
+    }
+
+    // Generate RHS, B = A*ones(n,1)
+    hB[k] = (double*)malloc((n1[k]+n2[k]) * sizeof(double));
+    hX[k] = (double*)malloc((n1[k]+n2[k]) * sizeof(double));
+    // B1 = [A, E]*ones(n,1)
+    for (rocblas_int i=0; i<n1[k]; i++) {
+      hB[k][i] = 0.0;
+      // B1 += A*ones(n1)
+      for (rocblas_int j=0; j<n1[k]; j++) {
+        hB[k][i] += hA[k][i+j*n1[k]];
+      }
+      // B1 += E*ones(n2)
+      for (rocblas_int j=0; j<n2[k]; j++) {
+        hB[k][i] += hE[k][i+j*n1[k]];
+      }
+    }
+    // B2 = [E', D]*ones(n,1)
+    for (rocblas_int i=0; i<n2[k]; i++) {
+      hB[k][n1[k]+i] = 0.0;
+      // B2 += E'*ones(n2)
+      for (rocblas_int j=0; j<n1[k]; j++) {
+        hB[k][n1[k]+i] += hE[k][j+i*n1[k]];
+      }
+      // B2 += D*ones(n2)
+      for (rocblas_int j=0; j<n2[k]; j++) {
+        hB[k][n1[k]+i] += hD[k][i+j*n2[k]];
+      }
+    }
+    // Allocate matrix & vector on device
+    // > matrix
+    hipMalloc((void**)&(dA[k]), sizeof(double) * n1[k]*n1[k]);
+    hipMalloc((void**)&(dE[k]), sizeof(double) * n1[k]*n2[k]);
+    hipMalloc((void**)&(dD[k]), sizeof(double) * n2[k]*n2[k]);
+    // > vector
+    int n = n1[k] + n2[k];
+    hipMalloc((void**)&(dB[k]), sizeof(double) * n);
+  } // End of generating matrix & vectors
+  if (fp) fclose(fp);
+
+
+  //
+  // !!!!! Outer Iterations !!!!!
+  //
+  for (int iter = 0; iter < niters; iter++) {
+    printf( "\n ===== iteration %d =====\n",iter );
+
+    // Copy matrix from host to device
+    for (int k=0; k<nb; k++) {
+      hipMemcpy(dA[k], hA[k], sizeof(double) * n1[k]*n1[k], hipMemcpyHostToDevice);
+      hipMemcpy(dE[k], hE[k], sizeof(double) * n1[k]*n2[k], hipMemcpyHostToDevice);
+      hipMemcpy(dD[k], hD[k], sizeof(double) * n2[k]*n2[k], hipMemcpyHostToDevice);
+    }
+
+    // !!! factor the 2-by-2 blocks !!!
+    double  one( 1.0);
+    double mone(-1.0);
+    for (int k=0; k<nb; k++) {
+      // factor the first diagonal blocks, R := chol(A)
+      int qid = k%nstreams;
+      rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n1[k], dA[k], n1[k], &dInfo[k]);
+    }
+    if (fence) hipDeviceSynchronize();
+    for (int k=0; k<nb; k++) {
+      // compute the off-diagonal factor, E := R^{-1}*E
+      int qid = k%nstreams;
+      rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                    rocblas_operation_transpose, rocblas_diagonal_non_unit,
+                    n1[k], n2[k], &one, dA[k], n1[k], dE[k], n1[k]);
+      // update the second diagonal, D -= E'*E
+      rocblas_dsyrk(handles[qid], rocblas_fill_upper, rocblas_operation_transpose,
+                    n2[k], n1[k], &mone, dE[k], n1[k], &one, dD[k], n2[k]);
+    }
+    if (fence) hipDeviceSynchronize();
+    for (int k=0; k<nb; k++) {
+      // factor the second diagonal block, chol(D)
+      int qid = k%nstreams;
+      rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n2[k], dD[k], n2[k], &dInfo[k]);
+    }
+
+    // !!! check (on stream-0) !!!
+    hipDeviceSynchronize();
+    // Copy vector from host to device
+    for (int k=0; k<nb; k++) {
+      int n = n1[k] + n2[k];
+      hipMemcpy(dB[k], hB[k], sizeof(double) * n, hipMemcpyHostToDevice);
+    }
+
+    // Solve [A,E; E' D] [x1;x2] = [b1;b2]
+    for (int k=0; k<nb; k++) {
+      int qid = 0; //k%nstreams;
+      int n = n1[k]+n2[k];
+      // --- step 1 --
+      // b(1) := L(A)\b(1)
+      rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                    rocblas_operation_transpose, rocblas_diagonal_non_unit,
+                    n1[k], nrhs, &one, dA[k], n1[k], &dB[k][0], n);
+
+      // --- step 2 --
+      // b(2) := b(2) - E'*b(1)
+      rocblas_dgemm(handles[qid], rocblas_operation_transpose, rocblas_operation_none,
+                    n2[k], nrhs, n1[k], &mone, dE[k], n1[k], &dB[k][0], n, &one, &dB[k][n1[k]], n);
+
+      // --- step 3 --
+      // b(2) = L(D)\b(2)
+      rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                    rocblas_operation_transpose, rocblas_diagonal_non_unit,
+                    n2[k], nrhs, &one, dD[k], n2[k], &dB[k][n1[k]], n);
+      // x(2) = U(D)\x(2)
+      rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                    rocblas_operation_none, rocblas_diagonal_non_unit,
+                    n2[k], nrhs, &one, dD[k], n2[k], &dB[k][n1[k]], n);
+
+      // --- step 4 --
+      // b(1) = b(1) - E*x(2)
+      rocblas_dgemm(handles[qid], rocblas_operation_none, rocblas_operation_none,
+                    n1[k], nrhs, n2[k], &mone, dE[k], n1[k], &dB[k][n1[k]], n, &one, &dB[k][0], n);
+
+      // --- step 5 --
+      // x(1) = U(A)\b(1)
+      rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                    rocblas_operation_none, rocblas_diagonal_non_unit,
+                    n1[k], nrhs, &one, dA[k], n1[k], &dB[k][0], n);
+    }
+
+    // Compute the residual norm
+    hipDeviceSynchronize();
+    for (int k=0; k<nb; k++) {
+      // Copy solution and error code to host
+      rocblas_int hInfo;
+      hipMemcpy(hX[k], dB[k], sizeof(double) * (n1[k]+n2[k]), hipMemcpyDeviceToHost);
+      hipMemcpy(&hInfo, &dInfo[k], sizeof(rocblas_int), hipMemcpyDeviceToHost);
+      if (verbose == 1) {
+        printf("A=[\n");
+        for (int i = 0; i < n1[k]; ++i) {
+          for (int j = 0; j < n1[k]; ++j) printf("%.16e ",hA[k][i+j*n1[k]]);
+          printf("\n");
+        }
+        printf("];\n");
+        printf("E=[\n");
+        for (int i = 0; i < n1[k]; ++i) {
+          for (int j = 0; j < n2[k]; ++j) printf("%.16e ",hE[k][i+j*n1[k]]);
+          printf("\n");
+        }
+        printf("];\n");
+        printf("D=[\n");
+        for (int i = 0; i < n2[k]; ++i) {
+          for (int j = 0; j < n2[k]; ++j) printf("%.16e ",hD[k][i+j*n2[k]]);
+          printf("\n");
+        }
+        printf("];\n");
+        printf("ixb=[\n");
+        for (int i = 0; i < n1[k]+n2[k]; ++i) printf("%d %.16e %.16e\n",i,hX[k][i],hB[k][i]);
+        printf("];\n");
+      }
+
+      if (hInfo == 0) {
+        double rnorm = 0.0;
+        double bnorm = 0.0;
+        printf("%d: Checking residual norms (n=%d+%d):\n",k,n1[k],n2[k]);
+        // residual norm for the first block row:
+        for (int i = 0; i < n1[k]; ++i) {
+          double Bi = hB[k][i];
+          bnorm += Bi*Bi;
+          for (int j = 0; j < n1[k]; ++j) {
+            Bi -= hA[k][i+j*n1[k]] * hX[k][j];
+          }
+          for (int j = 0; j < n2[k]; ++j) {
+            Bi -= hE[k][i+j*n1[k]] * hX[k][n1[k]+j];
+          }
+          rnorm += Bi*Bi;
+        }
+        double rnorm1 = std::sqrt(rnorm);
+        double bnorm1 = std::sqrt(bnorm);
+        printf( " 1: rnorm = %e / %e = %e\n",rnorm1,bnorm1,rnorm1/bnorm1 );
+        // residual norm for the second block row:
+        for (int i = 0; i < n2[k]; ++i) {
+          double Bi = hB[k][n1[k]+i];
+          bnorm += Bi*Bi;
+          for (int j = 0; j < n1[k]; ++j) {
+            Bi -= hE[k][j+i*n1[k]] * hX[k][j];
+          }
+          for (int j = 0; j < n2[k]; ++j) {
+            Bi -= hD[k][i+j*n2[k]] * hX[k][n1[k]+j];
+          }
+          rnorm += Bi*Bi;
+        }
+        rnorm = std::sqrt(rnorm);
+        bnorm = std::sqrt(bnorm);
+        printf( " 2: rnorm = %e / %e = %e\n",rnorm,bnorm,rnorm/bnorm );
+        double tol = 0.000000001;
+        if (rnorm > tol*bnorm) printf("%d: FAIL (%dx%d %e / %e = %e)\n",k,n1[k],n2[k], rnorm,bnorm,rnorm/bnorm);
+        else printf("%d: PASS\n",k);
+      } else {
+        printf("Cholesky factorization failed. Info code: %d\n", hInfo);
+      }
+      printf("\n");
+    }
   }
   // Free memory
   for (int k=0; k<nb; k++) {
