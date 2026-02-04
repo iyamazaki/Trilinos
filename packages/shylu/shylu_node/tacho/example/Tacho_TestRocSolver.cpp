0f5fd6d7ebc409f905a421e80548103daa9cf2ec
iyamazaki <iyamaza@sandia.gov>
2026-02-04 14:44:30 -0700

    Tacho: more fence options in TestRocSolver
    Signed-off-by: iyamazaki <iyamaza@sandia.gov>

--- packages/shylu/shylu_node/tacho/example/Tacho_TestRocSolver.cpp
+++ packages/shylu/shylu_node/tacho/example/Tacho_TestRocSolver.cpp
@@ -41,19 +41,26 @@ int main(int argc, char **argv) {
   int nstreams = 16; // number of streams
   int option = 0;
   char* filename = nullptr;
-  bool fence = false;
+  bool no_blas = false;     // drop off-diagonal block
+  bool fence = false;       // fence between potrf and blas calls
+  bool fence_all = false;   // fence after each kernel call
+  bool fence_po = false;    // fence after potrf
+  bool fence_blas = false;  // fence after blas call
   for (int i=0; i<argc; i++) {
-    if (0 == strcmp(argv[i], "--fence"))    fence = true;
-    if (0 == strcmp(argv[i], "--verbose"))  verbose = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--file"))     filename = argv[++i];
-    if (0 == strcmp(argv[i], "--option"))   option = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--n1"))       N1 = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--n2"))       N2 = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--nb"))       nb = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--niters"))   niters = atoi(argv[++i]);
-    if (0 == strcmp(argv[i], "--nstreams")) nstreams = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--fence"))      fence = true;
+    if (0 == strcmp(argv[i], "--fence_blas")) fence_blas = true;
+    if (0 == strcmp(argv[i], "--fence_po"))   fence_po = true;
+    if (0 == strcmp(argv[i], "--fence_all"))  fence_all = true;
+    if (0 == strcmp(argv[i], "--verbose"))    verbose = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--file"))       filename = argv[++i];
+    if (0 == strcmp(argv[i], "--option"))     option = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--n1"))         N1 = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--n2"))         N2 = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--no_blas"))    no_blas = true;
+    if (0 == strcmp(argv[i], "--nb"))         nb = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--niters"))     niters = atoi(argv[++i]);
+    if (0 == strcmp(argv[i], "--nstreams"))   nstreams = atoi(argv[++i]);
   }
-  printf( "\n n=%d+%d, nd=%d, nstreams=%d, option=%d %s\n",N1,N2,nb,nstreams,option,filename );
 
   // Create rocSOLVER/BLAS handle
   rocblas_handle *handles = (rocblas_handle*)malloc(nstreams * sizeof(rocblas_handle));
@@ -70,6 +77,26 @@ int main(int argc, char **argv) {
   }
 
   // Solving "nb" 2-by-2 SPD linear systems [A E; E' D]
+  FILE *fp = nullptr;
+  if (filename) {
+    printf( "\n READING block sizes from %s\n",filename );
+    fp = fopen(filename,"r");
+    if (!fp) {
+      printf( "\n ** FAILED to open %s **\n\n",filename );
+      exit(0);
+    }
+    nb = 0;
+    char c;
+    while (EOF != (c=getc(fp))) {
+      if ('\n' == c) ++nb;
+    }
+    printf( " > There are nb=%d lines <\n",nb );
+    rewind(fp);
+  }
+  // > Setup matrices/vectors
+  printf( "\n n=%d+%d, nd=%d, nstreams=%d, option=%d%s%s%s%s\n",N1,N2,nb,nstreams,option,
+              (fence ? ", with fence" : ""),(fence_po ? ", with fence potrf" : ""),
+              (fence_blas ? ", with fence blas" : ""),(fence_all ? ", with fence all" :""));
   // > on host
   double **hA = (double**)malloc(nb * sizeof(double*));
   double **hE = (double**)malloc(nb * sizeof(double*));
@@ -84,18 +111,13 @@ int main(int argc, char **argv) {
   double **hX = (double**)malloc(nb * sizeof(double*));
   double **dB = (double**)malloc(nb * sizeof(double*));
   // info
-  rocblas_int *dInfo;
-  HIP_CHECK(hipMalloc((void**)&dInfo, nb*sizeof(rocblas_int)));
+  rocblas_int *dInfo1, *dInfo2;
+  HIP_CHECK(hipMalloc((void**)&dInfo1, nb*sizeof(rocblas_int)));
+  HIP_CHECK(hipMalloc((void**)&dInfo2, nb*sizeof(rocblas_int)));
   // block sizes
   int *n1 = (int*)malloc(nb * sizeof(int));
   int *n2 = (int*)malloc(nb * sizeof(int));
 
-  // Setup matrices/vectors
-  FILE *fp = nullptr;
-  if (filename) {
-    printf( " READING block sizes from %s\n",filename );
-    fp = fopen(filename,"r");
-  }
   // Set random seed for determistic matrix blocks
   std::srand(1);
   for (int k=0; k<nb; k++) {
@@ -108,8 +130,10 @@ int main(int argc, char **argv) {
     }
     // Generate SPD matrices [A E; E' D]
     hA[k] = (double*)malloc((n1[k]*n1[k]) * sizeof(double));
-    hE[k] = (double*)malloc((n1[k]*n2[k]) * sizeof(double));
-    hD[k] = (double*)malloc((n2[k]*n2[k]) * sizeof(double));
+    if (n2[k] > 0) {
+      hE[k] = (double*)malloc((n1[k]*n2[k]) * sizeof(double));
+      hD[k] = (double*)malloc((n2[k]*n2[k]) * sizeof(double));
+    }
 
     // A, first diagonal blocks, and E, off-diagonal block
     for (rocblas_int i=0; i<n1[k]; i++) {
@@ -120,7 +144,7 @@ int main(int argc, char **argv) {
       hA[k][i+i*n1[k]] += (n1[k]+n2[k]);
       // E, off-diagonal block, n1-by-n2
       for (rocblas_int j=0; j<n2[k]; j++) {
-        hE[k][i+j*n1[k]] = double(std::rand()) / double(RAND_MAX);
+        hE[k][i+j*n1[k]] = (no_blas ? 0.0 : double(std::rand()) / double(RAND_MAX));
       }
     }
     // D, second diagonal block, and off-diagonal block
@@ -207,8 +231,10 @@ int main(int argc, char **argv) {
     // Allocate matrix & vector on device
     // > matrix
     HIP_CHECK(hipMalloc((void**)&(dA[k]), sizeof(double) * n1[k]*n1[k]));
-    HIP_CHECK(hipMalloc((void**)&(dE[k]), sizeof(double) * n1[k]*n2[k]));
-    HIP_CHECK(hipMalloc((void**)&(dD[k]), sizeof(double) * n2[k]*n2[k]));
+    if (n2[k] > 0) {
+      HIP_CHECK(hipMalloc((void**)&(dE[k]), sizeof(double) * n1[k]*n2[k]));
+      HIP_CHECK(hipMalloc((void**)&(dD[k]), sizeof(double) * n2[k]*n2[k]));
+    }
     // > vector
     int n = n1[k] + n2[k];
     HIP_CHECK(hipMalloc((void**)&(dB[k]), sizeof(double) * n));
@@ -225,9 +251,12 @@ int main(int argc, char **argv) {
     // Copy matrix from host to device
     for (int k=0; k<nb; k++) {
       HIP_CHECK(hipMemcpy(dA[k], hA[k], sizeof(double) * n1[k]*n1[k], hipMemcpyHostToDevice));
-      HIP_CHECK(hipMemcpy(dE[k], hE[k], sizeof(double) * n1[k]*n2[k], hipMemcpyHostToDevice));
-      HIP_CHECK(hipMemcpy(dD[k], hD[k], sizeof(double) * n2[k]*n2[k], hipMemcpyHostToDevice));
+      if (n2[k] > 0) {
+        HIP_CHECK(hipMemcpy(dE[k], hE[k], sizeof(double) * n1[k]*n2[k], hipMemcpyHostToDevice));
+        HIP_CHECK(hipMemcpy(dD[k], hD[k], sizeof(double) * n2[k]*n2[k], hipMemcpyHostToDevice));
+      }
     }
+    HIP_CHECK(hipDeviceSynchronize());
 
     // !!! factor the 2-by-2 blocks !!!
     double  one( 1.0);
@@ -235,24 +264,31 @@ int main(int argc, char **argv) {
     for (int k=0; k<nb; k++) {
       // factor the first diagonal blocks, R := chol(A)
       int qid = k%nstreams;
-      ROC_CHECK(rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n1[k], dA[k], n1[k], &dInfo[k]));
+      ROC_CHECK(rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n1[k], dA[k], n1[k], &dInfo1[k]));
+      if (fence_all || fence_po) HIP_CHECK(hipDeviceSynchronize());
     }
-    if (fence) HIP_CHECK(hipDeviceSynchronize());
-    for (int k=0; k<nb; k++) {
-      // compute the off-diagonal factor, E := R^{-1}*E
-      int qid = k%nstreams;
-      ROC_CHECK(rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
-                              rocblas_operation_transpose, rocblas_diagonal_non_unit,
-                              n1[k], n2[k], &one, dA[k], n1[k], dE[k], n1[k]));
-      // update the second diagonal, D -= E'*E
-      ROC_CHECK(rocblas_dsyrk(handles[qid], rocblas_fill_upper, rocblas_operation_transpose,
-                              n2[k], n1[k], &mone, dE[k], n1[k], &one, dD[k], n2[k]));
+    if (!no_blas) {
+      if (fence || fence_blas) HIP_CHECK(hipDeviceSynchronize());
+      for (int k=0; k<nb; k++) {
+        // compute the off-diagonal factor, E := R^{-1}*E
+        int qid = k%nstreams;
+        ROC_CHECK(rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
+                                rocblas_operation_transpose, rocblas_diagonal_non_unit,
+                                n1[k], n2[k], &one, dA[k], n1[k], dE[k], n1[k]));
+        if (fence_all || fence_blas) HIP_CHECK(hipDeviceSynchronize());
+
+        // update the second diagonal, D -= E'*E
+        ROC_CHECK(rocblas_dsyrk(handles[qid], rocblas_fill_upper, rocblas_operation_transpose,
+                                n2[k], n1[k], &mone, dE[k], n1[k], &one, dD[k], n2[k]));
+        if (fence_all || fence_blas) HIP_CHECK(hipDeviceSynchronize());
+      }
     }
-    if (fence) HIP_CHECK(hipDeviceSynchronize());
+    if (fence || fence_po) HIP_CHECK(hipDeviceSynchronize());
     for (int k=0; k<nb; k++) {
       // factor the second diagonal block, chol(D)
       int qid = k%nstreams;
-      ROC_CHECK(rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n2[k], dD[k], n2[k], &dInfo[k]));
+      ROC_CHECK(rocsolver_dpotrf(handles[qid], rocblas_fill_upper, n2[k], dD[k], n2[k], &dInfo2[k]));
+      if (fence_all || fence_po) HIP_CHECK(hipDeviceSynchronize());
     }
 
     // !!! check (on stream-0) !!!
@@ -263,6 +299,7 @@ int main(int argc, char **argv) {
       int n = n1[k] + n2[k];
       HIP_CHECK(hipMemcpy(dB[k], hB[k], sizeof(double) * n, hipMemcpyHostToDevice));
     }
+    HIP_CHECK(hipDeviceSynchronize());
 
     // Solve [A,E; E' D] [x1;x2] = [b1;b2]
     for (int k=0; k<nb; k++) {
@@ -273,11 +310,12 @@ int main(int argc, char **argv) {
       ROC_CHECK(rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
                               rocblas_operation_transpose, rocblas_diagonal_non_unit,
                               n1[k], nrhs, &one, dA[k], n1[k], &dB[k][0], n));
-
-      // --- step 2 --
-      // b(2) := b(2) - E'*b(1)
-      ROC_CHECK(rocblas_dgemm(handles[qid], rocblas_operation_transpose, rocblas_operation_none,
-                              n2[k], nrhs, n1[k], &mone, dE[k], n1[k], &dB[k][0], n, &one, &dB[k][n1[k]], n));
+      if (!no_blas) {
+        // --- step 2 --
+        // b(2) := b(2) - E'*b(1)
+        ROC_CHECK(rocblas_dgemm(handles[qid], rocblas_operation_transpose, rocblas_operation_none,
+                                n2[k], nrhs, n1[k], &mone, dE[k], n1[k], &dB[k][0], n, &one, &dB[k][n1[k]], n));
+      }
 
       // --- step 3 --
       // b(2) = L(D)\b(2)
@@ -289,11 +327,12 @@ int main(int argc, char **argv) {
                               rocblas_operation_none, rocblas_diagonal_non_unit,
                               n2[k], nrhs, &one, dD[k], n2[k], &dB[k][n1[k]], n));
 
-      // --- step 4 --
-      // b(1) = b(1) - E*x(2)
-      ROC_CHECK(rocblas_dgemm(handles[qid], rocblas_operation_none, rocblas_operation_none,
-                              n1[k], nrhs, n2[k], &mone, dE[k], n1[k], &dB[k][n1[k]], n, &one, &dB[k][0], n));
-
+      if (!no_blas) {
+        // --- step 4 --
+        // b(1) = b(1) - E*x(2)
+        ROC_CHECK(rocblas_dgemm(handles[qid], rocblas_operation_none, rocblas_operation_none,
+                                n1[k], nrhs, n2[k], &mone, dE[k], n1[k], &dB[k][n1[k]], n, &one, &dB[k][0], n));
+      }
       // --- step 5 --
       // x(1) = U(A)\b(1)
       ROC_CHECK(rocblas_dtrsm(handles[qid], rocblas_side_left, rocblas_fill_upper,
@@ -307,16 +346,17 @@ int main(int argc, char **argv) {
     // Copy vector from host to device
     for (int k=0; k<nb; k++) {
       // Copy solution and error code to host
-      rocblas_int hInfo;
+      rocblas_int hInfo1, hInfo2;
       HIP_CHECK(hipMemcpy(hX[k], dB[k], sizeof(double) * (n1[k]+n2[k]), hipMemcpyDeviceToHost));
-      HIP_CHECK(hipMemcpy(&hInfo, &dInfo[k], sizeof(rocblas_int), hipMemcpyDeviceToHost));
+      HIP_CHECK(hipMemcpy(&hInfo1, &dInfo1[k], sizeof(rocblas_int), hipMemcpyDeviceToHost));
+      HIP_CHECK(hipMemcpy(&hInfo2, &dInfo2[k], sizeof(rocblas_int), hipMemcpyDeviceToHost));
       if (verbose == 2) {
         printf("x%d=[\n",k);
         for (int i = 0; i < n1[k]+n2[k]; ++i) printf("%d %.16e %.16e\n",i,hX[k][i],hB[k][i]);
         printf("];\n");
       }
 
-      if (hInfo == 0) {
+      if (hInfo1 == 0 && hInfo2 == 0) {
         double rnorm = 0.0;
         double bnorm = 0.0;
         printf("%d: Checking residual norms (n=%d+%d):\n",k,n1[k],n2[k]);
@@ -354,7 +394,7 @@ int main(int argc, char **argv) {
         if (rnorm > tol*bnorm) printf("%d: FAIL (%dx%d %e / %e = %e)\n",k,n1[k],n2[k], rnorm,bnorm,rnorm/bnorm);
         else printf("%d: PASS\n",k);
       } else {
-        printf("Cholesky factorization failed. Info code: %d\n", hInfo);
+        printf("rocsolver_dpotrf FAILed with info = %d,%d\n", hInfo1,hInfo2);
       }
       printf("\n");
     }
@@ -363,11 +403,13 @@ int main(int argc, char **argv) {
   for (int k=0; k<nb; k++) {
     // matrix
     free(hA[k]);
-    free(hE[k]);
-    free(hD[k]);
     HIP_CHECK(hipFree(dA[k]));
-    HIP_CHECK(hipFree(dE[k]));
-    HIP_CHECK(hipFree(dD[k]));
+    if (n2[k] > 0) {
+      free(hE[k]);
+      free(hD[k]);
+      HIP_CHECK(hipFree(dE[k]));
+      HIP_CHECK(hipFree(dD[k]));
+    }
     // vector
     free(hX[k]);
     free(hB[k]);
@@ -386,7 +428,8 @@ int main(int argc, char **argv) {
   free(hX);
   free(hB);
   free(dB);
-  HIP_CHECK(hipFree(dInfo));
+  HIP_CHECK(hipFree(dInfo1));
+  HIP_CHECK(hipFree(dInfo2));
   for (int k=0; k<nstreams; k++) {
     ROC_CHECK(rocblas_destroy_handle(handles[k]));
     if (option != 0) {
